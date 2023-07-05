779def0b604c89992a298cc755e25dbc7690fa15
Pedro Arns Junior <pedroarnsjr@outlook.com>
2023-07-05 15:14:59 -0300

    Adicionando CSS no Next.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,8 @@
+import styles from '@/styles/Footer.module.css';
 export default function Footer(){
     return(
-      <>
+      <footer className={styles.footer}>
         <p>Curso de Next.js &copy; 2023</p>
-      </>  
+      </footer>  
     )
 }
